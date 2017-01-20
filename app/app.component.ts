7f2e30d6e3bdff8c434a4a7e1b2a1be9229052b2
Fornaroli Mario <fornaroli@eurisnet.it>
2017-01-20 22:38:38 +0100

    First Commit

---
+++ app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+export class Doctor {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>Doctor {{doctor.name}} details!</h2>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="doctor.name" placeholder="name">
+    </div>
+    `
+})
+export class AppComponent {
+  title = 'Doctor details';
+  doctor: Doctor = {
+    id: 1,
+    name: 'Bianchi'
+  };
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
